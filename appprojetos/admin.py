e0f94da1fdd6d7a50461e315e0928ec9cf367088
Hallessandro <hd.jesus93@gmail.com>
2016-06-11 18:02:13 -0300

    Testando modelos

--- appprojetos/admin.py
+++ appprojetos/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(Professor)
+admin.site.register(Projeto_pesquisa)
+admin.site.register(Atividade)
+admin.site.register(Nv_desenvolvimento)
+admin.site.register(Membros)
--- appprojetos/models.py
+++ appprojetos/models.py
@@ -4,13 +4,24 @@ class Membros(models.Model):
     nome = models.CharField("Nome", max_length=150)
     matricula = models.CharField("Matricula", max_length=12)
 
+    def __str__(self):
+        return self.nome
+
 class Professor(Membros):
     area_atuacao = models.CharField("Area de atuação", max_length=255)
 
 class Nv_desenvolvimento(models.Model):
-    estagio = models.CharField("Estagio desenvolvimento", max_length=150)
+    Estagio = (
+        ('IN', 'Iniciada'),
+        ('DS', 'Desenvolvimento'),
+        ('CO', 'Concluida')
+    )
     dt_registro = models.DateField("Data do registro")
     descricao = models.CharField("Descrição", max_length=255)
+    estagio = models.CharField(max_length=2, choices=Estagio)
+
+    def __str__(self):
+        return self.estagio
 
 class Atividade(models.Model):
     descricao = models.CharField("Descrição", max_length=255)
@@ -18,6 +29,9 @@ class Atividade(models.Model):
     dt_termino = models.DateField("Data de termino da atividade")
     custo = models.FloatField("Custo da atividade")
     dsvol = models.ForeignKey(Nv_desenvolvimento, on_delete=models.PROTECT, verbose_name="Nivel de desenvolvimento")
+
+    def __str__(self):
+        return self.descricao
 
 class Projeto_pesquisa(models.Model):
     titulo = models.CharField("Titulo", max_length=255)
@@ -29,5 +43,6 @@ class Projeto_pesquisa(models.Model):
     atividade = models.ForeignKey(Atividade, on_delete=models.PROTECT, verbose_name="Atividade desenvolvida")
     membros = models.ManyToManyField(Membros)
 
-
+    def __str__(self):
+        return self.titulo
 
